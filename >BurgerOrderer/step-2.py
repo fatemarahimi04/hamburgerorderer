4d4d8ef6a904d932072f152909a63096b12613a6
Rezvanhejafari <reja24@student.bth.se>
2024-09-20 10:08:14 +0200

    Update step-2.py

--- >BurgerOrderer/step-2.py
+++ >BurgerOrderer/step-2.py
@@ -1,1 +1,18 @@
+Vår Klassiska Tillbehör 
+Pommes Frites: Krispiga och gyllene, serverad med ketchup och majonnäs.
+Sötpotaatispommes: Söt och krispig smak.
+Lökringar: Friterade lökringar med en krispig yta och mjuk insida.
+
+Frächa Sallader:
+Coleslaw: En krämig kålsallad,isbergsallad med morötter och en touch av vinäger.
+  Grönasallad: Blandade gröna blad, tomater, gurka och mjas. 
+  Gaesarsallad: Romansallad, krutonger, parmesanost och och casersås. 
+
+  Dipp-Såser
+  BBQ-sås: En rökig och söt sås som passar perfekt till hamburgare och pommes.
+  Chilimajonnäs: En kyrddig majonnäs för en godare smak.
+  Aioli: En krämig vitlöksås som ger extra smakdimension.
+  Jalapeño poppers: Fyllda jalapeños med ost, friterade till perfektion.
+  Saltgurka: syrliga gurkor som ger en fräsch kontrast till hamburagarna. 
+  
 
