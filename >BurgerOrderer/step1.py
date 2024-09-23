bfddc324b94b458fdda80719da56bc4ce1fc3777
fatemarahimi04 <fatema.rahimi004@gmail.com>
2024-09-23 11:59:51 +0200

    Update step1.py
    i have started my code to order burgers, created three burgers.

--- >BurgerOrderer/step1.py
+++ >BurgerOrderer/step1.py
@@ -1,1 +1,29 @@
+from flask import Flask
+from flask import request
+import os
+import request
 
+app = Flask(__name__)
+
+Burgers= [{"name": "DemureChicken"},
+          {"name": "HeroBurger"},
+          {"name": "CutieHalloumi"}]
+
+def getBurgers():
+  return Burgers;
+
+
+def presentFronypage():
+  pg = "Welcome to BurgerOrderer"
+  pg += "<P><UL>"
+
+def visa_meny():
+  menu = {
+    ("1) DemureChiken")
+    ("2) HeroBurger")
+    ("3) CutieHalloumi")
+  }
+
+    
+  
+  
