e08d8d40e469da412b192ba141793ab891221489
fatemarahimi04 <fatema.rahimi004@gmail.com>
2024-09-24 23:09:03 +0200

    Update step1.py
    skrivit klar koden för burgerorderer. ska kolla vidare om det behövs skrivas på något extra.

--- >BurgerOrderer/step1.py
+++ >BurgerOrderer/step1.py
@@ -1,26 +1,68 @@
-def visa_meny():
-    menu = {
-        "1) DemureChicken": 40,
-        "2) HeroBurger": 60,
-        "3) CutieVeggie": 50
+burgare_meny = {
+    "DemureChicken": {
+        "pris": 40,
+        "ingredienser": ["kyckling", "sallad", "tomat", "majonnäs"]
+    },
+    "HeroBurger": {
+        "pris": 60,
+        "ingredienser": ["nöttkött", "ost", "lök", "sallad", "tomat", "senap"]
+    },
+    "CutieVeggie": {
+        "pris": 50,
+        "ingredienser": ["vegetariskt kött", "ost", "sallad", "lök", "tomat", "avokado"]
     }
+}
+
+def visa_meny():
     print("Varmt välkommen till burgarmenyn!")
     print("Dessa burgare har vi idag:\n")
-
-    for burger, price in menu.items():
-        print(f"{burger}: {price} kr")
+   
+    for burger, info in burgare_meny.items():
+        print(f"{burger}: {info['pris']} kr")
+        print(f"Innehåll: {', '.join(info['ingredienser'])}\n")
 
 def beställ_burgare():
-    menu = {
-        "1) DemureChicken": 40,
-        "2) HeroBurger": 60,
-        "3) CutieVeggie": 40
-    }
-    while True:
-        val = input("\nVilken burgare är du sugen på idag? Ange numret: ")
-        if val in menu:
-            print(f"\nDu har beställt {val}. Det kostar {menu[val]} kr.")
+    while True: 
+        val = input("\nVilken burgare är du sugen på idag? Ange namn(eller 'avsluta' för att avsluta): ")
+        
+        if val == "avsluta":
+            print("Tack för ditt besök!")
+            break
+            
+        if val in burgare_meny:
+            burgare = burgare_meny[val]
+            print(f"\nDu har valt {val}. Det kostar {burgare['pris']} kr.")
+            print(f"Ingredienser: {', '.join(burgare['ingredienser'])}")
+            
+            ta_bort_ingredienserna(burgare['ingredienser'])
+        else:
+            print("Ogiltigt val, försök igen.")
 
+def ta_bort_ingredienser(ingredienser): 
+    borttagna_ingredienser = []
+    while True:
+        val = input(f"\nVill du ta bort någon ingrediens? (skriv namnet eller 'klar' för att fortsätta): ")
+        
+        if val == "klar":
+            break
+            
+        if val in ingredienser:
+            ingredienser.remove(val)
+            borttagna_ingredienser.append(val)
+            print(f"{val} har tagits bort.")
+        else:
+            print("Den ingrediensen finns inte på burgaren.")
+            
+    print(f"\nDin slutgiltiga burgare innehåller: {', '.join(ingrediensier)}.")
+    if borttagna_ingredienser:
+        print(f"Du har tagit bort: {', '.join(borttagna_ingredienser)}.\n")
+
+visa_meny()
+beställ_burgare()
+
+        
     
-  
+            
+
+
   
